d980955e1ef48d932a3ff77553ea44fa381e2e4b
Sebastián Silva <seebassilva@gmail.com>
2020-07-24 22:06:36 -0500

    Create register component

--- frontend/memories-app/src/app/register/register.component.ts
+++ frontend/memories-app/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private api: ApiService,
+    private router: Router
+  ){
+    this.registerForm = this.formBuilder.group({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.minLength(8))
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void{
+    if ( !this.registerForm.valid ){
+      return;
+    }
+    this.api.register(this.registerForm.value).subscribe( data => {
+      localStorage.setItem('user', JSON.stringify( data.token ));
+      if ( data.token ){
+        // If register is correct, navigate to users
+        this.router.navigate(['/memories']);
+      }
+    }, error => {
+      Swal.fire(
+        'There was an error!',
+        'Please try with a different username.',
+        'error'
+      );
+   });
   }
 
 }
